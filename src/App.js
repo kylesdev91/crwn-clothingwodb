6eb9c3abe260ce77b5b92f3e8d27884195a38adc
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-05-29 08:27:56 -0500

    React Lecture 85

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './routes/navigation/navigation.component';
 
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1> the navigation bar I am</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return <h1>I am the shop page.</h1>;
