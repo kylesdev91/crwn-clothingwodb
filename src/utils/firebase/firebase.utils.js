ca6464f7b535f8736a356b8f7fbe5796a623f0b5
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-05-30 18:02:11 -0500

    ???

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -76,12 +76,3 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 };
 
 export const signOutUser = async () => await signOut(auth);
-
-export const onAuthStateChangedListener = (callback) =>
-  onAuthStateChanged(auth, callback, errorCallback, completedCallback);
-
-/**
- * {
- * next: callback
- * }
- */
