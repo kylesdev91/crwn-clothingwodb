94ede992270c0aeb91aa57dc2946963b0e7bcd53
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-10 21:03:19 -0500

    Removed the home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-
-import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 // import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
@@ -10,7 +8,6 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
 
         <Route path='checkout' element={<Checkout />} />
